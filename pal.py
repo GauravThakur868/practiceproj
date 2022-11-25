20c0e52cdb24fe6894c96a999c727848a5c1e706
LAPTOP-2O353BVN\gt267 <GT267200@gmail.com>
2022-11-25 23:21:14 +0530

    m

---
+++ pal.py
@@ -0,0 +1,4 @@
+a = int(input())
+n = 
+for i in range(2,n):
+    
--- palin.py
+++ palin.py
@@ -1,11 +1,7 @@
 a = int(input())
 b = str(a)
-x = 1
-d = 0
-def reverse(z):
-    return z[::-1]
-c = reverse(b)
-while x <= a:
-    print(x)
-    x = x+1
-    
+rev = 0
+for i in range(2,a):
+    if i % 2 != 0:
+        if str(i) == str(i)[::-1]:
+            print(i)
