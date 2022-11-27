062847678b6fe39502f380652dbd8393ea9d287e
LAPTOP-2O353BVN\gt267 <GT267200@gmail.com>
2022-11-27 22:11:05 +0530

    1

--- palin.py
+++ palin.py
@@ -1,7 +1,10 @@
 a = int(input())
 b = str(a)
-rev = 0
-for i in range(2,a):
+c = a -1
+d = 0
+print(2)
+for i in range(2,a**10):
     if i % 2 != 0:
-        if str(i) == str(i)[::-1]:
+        if str(i) == str(i)[::-1] and c > 0:
             print(i)
+            c = c-1
