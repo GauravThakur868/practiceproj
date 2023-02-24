593e3330de7d75a0db7e326f20a13d9efcf0e634
GAURAV <gt267200@gmail.com>
2023-02-24 23:32:54 +0530

    3

--- palin.py
+++ palin.py
@@ -6,5 +6,6 @@ print(2)
 for i in range(2,a**10):
     if i % 2 != 0:
         if str(i) == str(i)[::-1] and c > 0:
-            print(i)
-            c = c-1
+            if i%2!=0 and i%2!=0 and i%3!=0 and i%4!=0 and i%5!=0 and i%6!=0 and i%7!=0 and i%8!=0 and i%9!=0:
+                print(i)
+                c = c-1
